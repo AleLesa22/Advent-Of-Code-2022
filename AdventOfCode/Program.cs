9653cc6b9474bf5de29dc11d98ab68cf70b24e02
Aleksandar Vidakovic <aleksandar.vidakovic7422@gmail.com>
2022-12-01 11:27:21 +0100

    Elf with most calories, and sum of calories for the top three elves

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] lines = System.IO.File.ReadAllLines(@"C:\Users\avidakovic\Desktop\keynotes.txt");
+        Console.WriteLine("Contents of Elfs.txt = ");
+
+        int sum = 0;
+        int max = int.MinValue;
+
+        List<int> Elves = new List<int>();
+
+        foreach (string line in lines)
+        {
+            Console.WriteLine("\t" + line);
+            if (string.IsNullOrEmpty(line))
+            {
+                Elves.Add(sum);
+                sum = 0;
+            }
+            else
+            {
+                sum += Convert.ToInt32(line);
+            }
+        }
+
+        for (int i = 0; i < Elves.Count; i++)
+        {
+            if (Elves[i] > max)
+            {
+                max = Elves[i];
+            }
+        }
+        Console.WriteLine(max);
+
+
+        //SumOfTopThreeElves
+        var sumOfTopThree = Elves.OrderByDescending(x => x).Take(3).Sum();
+        Console.WriteLine("The sum of calories carried by the top three elves:");
+        Console.WriteLine(sumOfTopThree);
+        Console.ReadLine();
+    }
+}
